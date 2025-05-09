e84ebdef7b6a50471b3f62a79896d41188c46151
BilalShahid-13 <bcsm-f21-249@superior.edu.pk>
2025-05-09 20:47:27 +0500

    fix tablist styles

--- src/app/admin/components/craftersManagment.jsx
+++ src/app/admin/components/craftersManagment.jsx
@@ -48,31 +48,34 @@ export default function craftersManagment() {
 
   return (
     <>
-      <Tabs defaultValue="2">
+      <Tabs defaultValue="0">
         <TabsList
-          className={`flex flex-row gap-3 mt-3 mb-6
-          max-xl:grid max-lg:grid max-lg:place-items-center
-             max-xl:grid-cols-2 bg-transparent
-             max-xl:place-items-center
-          ${width ? 'max-lg:grid-cols-2' : 'max-lg:grid-cols-1'}
-             max-sm:grid max-sm:grid-cols-1 w-auto
-               h-auto max-lg:w-full`}
+          className={`flex flex-row flex-wrap justify-center items-center
+    gap-3 mt-3 mb-6
+    max-xl:grid max-xl:grid-cols-2 max-xl:place-items-center
+    max-lg:grid max-lg:place-items-center
+    ${width ? 'max-lg:grid-cols-2' : 'max-lg:grid-cols-1'}
+    max-sm:grid max-sm:grid-cols-1
+    bg-transparent h-auto w-full`}
         >
           {craftersManagmentList.map((items, index) => (
             <TabsTrigger
               key={index}
               value={`${index}`}
-              className={`h-auto cursor-pointer
-dark:data-[state=active]:border-[#ff7e6e]
-              ${width ? 'max-lg:w-fit max-xl:w-full' : 'max-lg:w-full'}
-                   p-0 m-0 w-fit max-sm:w-full
-                   dark:data-[state=active]:bg-transparent
-`}
+              className={`
+        w-full h-auto p-0 m-0 cursor-pointer
+        dark:data-[state=active]:bg-transparent
+        rounded-xl transition-all
+        dark:data-[state=active]:border-[#ff7e6e]
+        max-sm:w-full
+      `}
             >
+
               <CustomCard name={items.name} Icon={items.Icon} />
             </TabsTrigger>
           ))}
         </TabsList>
+
 
         {components.map((Items, index) => (
           <TabsContent key={index} value={`${index}`} className="mt-6">
